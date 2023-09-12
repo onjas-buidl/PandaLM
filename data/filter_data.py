267a1cae3fb360c0e77759fc735abec0b91409c3
onjas <onjas.buidl@gmail.com>
2023-09-12 14:56:41 -0700

    simple analysis

--- data/filter_data.py
+++ data/filter_data.py
@@ -3,13 +3,13 @@ import json, pandas as pd
 with open('data/testset-v1.json', 'r') as f:
     test_cases = json.load(f)
 
-
 df = pd.DataFrame(test_cases)
 
 df.motivation_app.value_counts()
 
 # filter df by motivation_app in a list of values
-df2 = df[df.motivation_app.isin(['Wolfram alpha', 'Messenger', 'Grammarly', 'https://cohere.ai/', 'Google Meet'])]
+df2 = df[df.motivation_app.isin([
+    'Wolfram alpha', 'Messenger', 'Grammarly', 'https://cohere.ai/', 'Google Meet'])]
 # cohere -> summary
 # wolfram -> math
 # grammarly -> grammar
@@ -19,3 +19,18 @@ df2 = df[df.motivation_app.isin(['Wolfram alpha', 'Messenger', 'Grammarly', 'htt
 
 # export df2 to json format and save it to data/testset-filtered-141.json
 df2.to_json('data/testset-filtered-141.json', orient='records')
+
+#%%
+################## analyzing new, filtered, results
+with open('data/testset-filtered-141-results.json', 'r') as f:
+    test_cases = json.load(f)
+
+df = pd.DataFrame(test_cases)
+
+# mean of PandaLM_result by value of motivation_app
+df.groupby('motivation_app').PandaLM_result.mean()
+df.groupby('motivation_app').PandaLM_result.var()
+
+# see what's the percentage of 1 for PandaLM_result for each motivation_app group
+df.groupby('motivation_app').PandaLM_result.value_counts(normalize=True)
+
